b6bb3af2ed762d2d2bcdb1583bcd87455660f92d
Matthew Dembinski <matthewdembinski@gmail.com>
2024-02-22 18:22:34 -0700

    Initial Commit

---
+++ cwc-project-tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cwc-project-tracker
+//
+//  Created by Matthew Dembinski on 2/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cwc-project-tracker/cwc_project_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  cwc_project_trackerApp.swift
+//  cwc-project-tracker
+//
+//  Created by Matthew Dembinski on 2/22/24.
+//
+
+import SwiftUI
+
+@main
+struct cwc_project_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
