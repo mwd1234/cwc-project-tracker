a0eb9ec81d45bcc0c7bb4b81b63c65dbfb10e764
Matthew Dembinski <matthewdembinski@gmail.com>
2024-02-27 16:42:07 -0700

    ADD: cross image, configure colors, and configure fonts

---
+++ cwc-project-tracker/Helpers/FontHelper.swift
@@ -0,0 +1,20 @@
+//
+//  FontHelper.swift
+//  cwc-project-tracker
+//
+//  Created by Matthew Dembinski on 2/27/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Font {
+    
+    static var screenHeading = Font.system(size: 36, weight: .bold)
+    static var bigHeadline = Font.system(size: 15, weight: .bold)
+    static var smallHeadline = Font.system(size: 10, weight: .bold)
+    static var featuredNumber = Font.system(size: 18, weight: .bold)
+    static var featuredText = Font.system(size: 12, weight: .bold)
+    static var regularText = Font.system(size: 10, weight: .bold)
+    static var captionText = Font.system(size: 8, weight: .bold)
+}
